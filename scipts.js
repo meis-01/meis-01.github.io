9852ab0917add397f9b1a2367d32a8a63bf31dbe
Meisam <meisam@tower.speedport.ip>
2024-08-04 18:51:26 +0200

    testing

---
+++ scipts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.querySelector('a');
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('Link clicked!');
+    });
+});
