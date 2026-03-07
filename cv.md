bc8f403c319dfc61445ba08c34e762bafdc00a4a
Meisam Ashraf <meisash@gmail.com>
2026-03-07 22:21:04 +0100

    cleaning

--- cv.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: CV
----
-Full CV available here:
-
-[Download PDF](assets/cv/Meisam_Ashraf_CV.pdf)
--- scipts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const link = document.querySelector('a');
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        alert('Link clicked!');
-    });
